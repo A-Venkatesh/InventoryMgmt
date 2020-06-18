f5defe2fd08f936592e1b7aca7e2e3e3970766b8
A <670322@cognizant.com>
2020-06-19 00:05:00 +0530

    product data module

---
+++ src/app/shared/services/product.ts
@@ -0,0 +1,12 @@
+export interface Product {
+    pID: string;
+    Name: string;
+    Price: number;
+    MRP: number;
+    Description: string;
+    Category: string;
+    SubCat: [];
+    AvlQty: number;
+    Picture: [];
+
+}
