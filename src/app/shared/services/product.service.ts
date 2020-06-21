87c8fd837c1efc1aa8553d9fb6c1e5c1fb35690f
A <670322@cognizant.com>
2020-06-21 18:31:27 +0530

    product service addedd

---
+++ src/app/shared/services/product.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { FormControl, FormGroup } from "@angular/forms";
+import { AngularFirestore } from '@angular/fire/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private firestore: AngularFirestore) {}
+
+
+  //Firestore CRUD actions example
+  createProduct(data) {
+    return new Promise<any>((resolve, reject) => {
+      this.firestore
+        .collection("products")
+        .add(data)
+        .then(res => {}, err => reject(err));
+    });
+  }
+
+  updateProduct(data) {
+    return this.firestore
+      .collection("products")
+      .doc(data.payload.doc.id)
+      .set({ completed: true }, { merge: true });
+  }
+
+  getProducts() {
+    return this.firestore.collection("products").snapshotChanges();
+  }
+
+  deleteProduct(data) {
+    return this.firestore
+      .collection("products")
+      .doc(data.payload.doc.id)
+      .delete();
+  }
+}
