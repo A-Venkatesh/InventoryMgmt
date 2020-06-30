5073b764159db99538e445f86a31d0a4419458c4
A <670322@cognizant.com>
2020-06-30 19:21:02 +0530

    check device and act

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,18 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from "../../auth.service";
+import { DeviceDetectorService } from 'ngx-device-detector';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  public deviceInfo = null;
+  public isMobilevar = false;
+  public isTabletvar = false;
+  public isDesktopvar = false;
 
-
-  constructor(public authService: AuthService) {
+  constructor(public authService: AuthService, private deviceService: DeviceDetectorService) {
+    this.detectDevice();
+    this.isMobile();
+    this.isTablet();
+    this.isDesktop();
   }
 
   ngOnInit() {
 
+  }
+  public detectDevice() {
+    this.deviceInfo = this.deviceService.getDeviceInfo();
+  }
+ 
+  public isMobile() {
+    this.isMobilevar = this.deviceService.isMobile();
+  }
+ 
+  public isTablet() {
+    this.isTabletvar = this.deviceService.isTablet();
+  }
+ 
+  public isDesktop() {
+    this.isDesktopvar = this.deviceService.isDesktop();
   }
 
 }
