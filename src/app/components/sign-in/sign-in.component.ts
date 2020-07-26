d6a8482e2b9e7d79c1ff38d79325ce24df632e61
A <670322@cognizant.com>
2020-07-26 21:49:39 +0530

    signin bug fixed

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../shared/services/auth.service';
-import {MatSnackBar} from '@angular/material/snack-bar';
+import { MatSnackBar } from '@angular/material/snack-bar';
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
@@ -8,9 +8,9 @@ import {MatSnackBar} from '@angular/material/snack-bar';
 })
 
 export class SignInComponent implements OnInit {
-keyWord = 'deliveryyaar';
-message = '';
-action = '';
+  keyWord = 'deliveryyaar';
+  message = '';
+  action = '';
   constructor(
     private snackBar: MatSnackBar,
     public authService: AuthService
@@ -18,17 +18,19 @@ action = '';
 
   ngOnInit() { }
 
-  signinCheck(name: string, pass: string){
-if (name.search(this.keyWord) === -1) {
-  this.message = ' You dont have admin access';
-  this.action = 'Ask Admin';
-} else {
-  this.message = 'Welcome Back';
-  this.action = 'Boss';
+  signinCheck(name: string, pass: string) {
+    if (!name.toLowerCase().startsWith(this.keyWord)) {
+      this.message = ' You dont have admin access';
+      this.action = 'Ask Admin';
+      this.openSnackBar(this.message, this.action);
+    } else {
+      this.message = 'Welcome Back';
+      this.action = 'Boss';
+      this.authService.SignIn(name, pass).then(result => {
+        this.openSnackBar(this.message, this.action);
+      });
+    }
 
-  this.authService.SignIn(name, pass);
-}
-this.openSnackBar(this.message, this.action);
   }
 
   openSnackBar(message: string, action: string) {
